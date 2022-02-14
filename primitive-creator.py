5d64b2ae44e059d0a685b6a4389794e8e307fbe3
VoxUrsa <lifeafternine@gmail.com>
2022-02-14 02:35:05 -0800

    Update primitive-creator.py
    Added Sphere Creation by replicating and editing Cube Creation

--- primitive-creator.py
+++ primitive-creator.py
@@ -10,9 +10,10 @@ class MR_Window(object):
         
         # "self" initial paramaters    
         self.window = "MR_Window"
-        self.title = "Cube Creator"
+        self.title = "Primitive Creator"
         self.size = (400, 400)
-            
+        self.cubeCreator = 'Cube Creator'
+        self.sphereCreator = 'Sphere Creator'           
         # Enforce uniqueness of tool window
         if cmds.window(self.window, exists = True):
             cmds.deleteUI(self.window, window=True)
@@ -23,6 +24,9 @@ class MR_Window(object):
         cmds.columnLayout(adjustableColumn = True)
         
         cmds.text(self.title)
+        cmds.separator(height=40)
+
+        cmds.text(self.cubeCreator)
         cmds.separator(height=20)
         
         self.cubeName = cmds.textFieldGrp(label= 'Cube Name: ')
@@ -30,10 +34,23 @@ class MR_Window(object):
         self.cubeSubdivs = cmds.intSliderGrp(field=True, label='Sub Divs', minValue=1, maxValue=32, value=1)
         
         self.cubeCreateBtn = cmds.button(label = 'Create Cube', command=self.createCube)
-        
+
+
 # We are attempting to assign hotkeys to trigger this event but we are not doing a good job of it. Will research in depth.
 #        cmds.nameCommand('triggerHotkeyEvent', annotation='Select Circle Tool', command='python("self.createCube")')
 #        cmds.hotkey( keyShortcut='j', name='triggerHotkeyEvent')
+
+
+        cmds.separator(height=20)
+        cmds.text(self.sphereCreator)
+        cmds.separator(height=20)
+        self.sphereName = cmds.textFieldGrp(label= 'Sphere Name: ')
+        self.sphereRadius = cmds.floatFieldGrp( numberOfFields=1, label='Radius: ', value1=1)
+        self.sphereSubdivs = cmds.intSliderGrp(field=True, label='Sub Divs', minValue=4, maxValue=32, value=1)
+        self.sphereCreateBtn = cmds.button(label = 'Create Sphere', command=self.createSphere)
+
+
+
 
         # Display new window
         cmds.showWindow()
@@ -52,6 +69,19 @@ class MR_Window(object):
      
         #Human readable output for testing and verification. All attributes will print to console.
         print('Name: ' + name + '\n' + 'Width: ' + str(width) + '\n' + 'Height: ' + str(height) + '\n' + 'Depth: ' + str(depth) + '\n' + 'Subdivisions: ' + str(subdivs))
+        
+    def createSphere(self, *args):
+        
+        name = cmds.textFieldGrp(self.sphereName, query=True, text=True)
+        
+        radius = cmds.floatFieldGrp(self.sphereRadius, query=True, value1=True)
+        
+        subdivs = cmds.intSliderGrp(self.sphereSubdivs, query=True, value=True)
+        
+        cmds.polySphere(name=name, radius=radius, subdivisionsX=subdivs, subdivisionsY=subdivs)
+     
+        #Human readable output for testing and verification. All attributes will print to console.
+        print('Name: ' + name + '\n' + 'Radius: ' + str(radius) + '\n' + 'Subdivisions: ' + str(subdivs))
     
 # Create an instance of the window                                   
 myWindow = MR_Window()
